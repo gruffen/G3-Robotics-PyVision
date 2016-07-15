e8ce74c136d1aef0c2bff6172ff6a168ce9d1219
Alex L <lexi431@gmail.com>
2016-07-15 15:37:34 -0400

    Added configuration launch file

---
+++ startVision.sh
@@ -0,0 +1,5 @@
+# Sets the appropriate camera exposure and other settings before running the vision processing code
+
+v4l2-ctl -d /dev/video1 -c exposure_auto=1 -c exposure_absolute=5
+chmod +x new_find_high_goal.py
+python new_find_high_goal.py
